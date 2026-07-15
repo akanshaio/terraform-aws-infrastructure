114270caa48a83a1a6a3b1d70c8149ed8cdaeb8e
akanshaio <akansha.js.171@outlook.com>
2026-07-15 12:57:49 +0530

    Migrate Terraform state to remote S3 backend

---
+++ main.tf
@@ -0,0 +1,9 @@
+ terraform {
+  backend "s3" {
+    bucket         = "akansha-tf-state"
+    key            = "network/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
